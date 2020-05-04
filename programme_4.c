ae545654c2d8c91dbbab00ae78dfbfa80ea52ee8
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-04 23:40:44 +0530

    Add files via upload

---
+++ programme_4.c
@@ -0,0 +1,13 @@
+// Hii there !! this is a simple calculator made using arithmetic operators
+main()
+{
+    float a , b ;
+    printf("The values of and b respectively are = ");
+    scanf("%f/%f", &a , &b );
+    getch();
+    printf("Addition of a and b = %0.2f \n", a+b );
+    printf("Subtraction of a and b = %0.2f \n", a-b );
+    printf("Multiplication of a and b = %0.2f \n", a*b );
+    printf("The whole number part of division when a/b = %0.2f \n ", a/b);
+    getch();
+}
