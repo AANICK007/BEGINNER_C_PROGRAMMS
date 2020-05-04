28ddb8bb0b74f60f2fc120c815574f7193b65b22
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-04 23:02:08 +0530

    Add files via upload

---
+++ programme_2.c
@@ -0,0 +1,8 @@
+// Hii there !! this programme is just addition of two numbers when data is already given
+main()
+{
+    int a = 2 , b = 3 ;
+    printf("Sum of a and b is %d ", a+b );
+    getch();
+}
+
