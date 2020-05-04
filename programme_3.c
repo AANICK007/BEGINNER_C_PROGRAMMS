e433c0b4526cb1b91c3cb41d9ede52ed7943962a
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-04 23:15:40 +0530

    Add files via upload

---
+++ programme_3.c
@@ -0,0 +1,12 @@
+// Hii there !! this is a simnple addition programme with input given by user
+main()
+{
+    int a , b ;
+    printf("The values of a and b respectively are = ");
+    scanf("%d/%d", &a,&b);
+    // please give / between the values of a and b in output
+    getch();
+    printf("The sum of a and b = %d ", a+b );
+    getch();
+}
+
