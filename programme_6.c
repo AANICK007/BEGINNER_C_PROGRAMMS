fa7f413ca7d392dc3f190d7e1e45483ab5660348
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-05 11:35:24 +0530

    Add files via upload

---
+++ programme_6.c
@@ -0,0 +1,12 @@
+// Hii there !! this is a simple program to calculate area of the circle
+// Area of circle = 3.143*radius*radius
+main()
+{
+    int radius , area ;
+    printf("The radius of the circle = ");
+    scanf("%d" , &radius );
+    getch();
+    area= 3.14*(radius*radius) ;
+    printf("The area of the circle is = %d", area );
+    getch();
+}
