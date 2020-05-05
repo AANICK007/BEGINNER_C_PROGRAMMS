c98632ff0d1086b3a146ae732ae9100e2fd4ff43
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-05 12:13:24 +0530

    Add files via upload

---
+++ programme_8.c
@@ -0,0 +1,9 @@
+// Hii there !! this is s program to print ascii code of characters
+main()
+{
+    char c ;
+    printf("Enter any character to print its ASCII code = ");
+    scanf("%c", &c );
+    printf("The desired ASCII code of character %c is = %d ", c , c );
+    getch();
+}
