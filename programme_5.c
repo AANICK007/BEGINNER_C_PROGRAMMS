ac14404b8f281bda35e6f623f2158476249ea689
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-05 11:25:42 +0530

    Add files via upload

---
+++ programme_5.c
@@ -0,0 +1,11 @@
+// Hii there !! this is a programme to calculate the area of a rectangle
+// Area of a rectangle = length*breadth
+main()
+{
+    int length , breadth ;
+    printf("The respective length and breadth of the rectangle are = ");
+    scanf("%d/%d", &length , &breadth );
+    getch();
+    printf("The area of the given rectangle = %d", length*breadth );
+    getch();
+}
