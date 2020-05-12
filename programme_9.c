5798c01e8b38d7720eda1416a631151eebaa1dfe
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-12 16:04:41 +0530

    Add files via upload

---
+++ programme_9.c
@@ -0,0 +1,11 @@
+// This is a programme to print format specifiersa
+#include<conio.h>
+#include<stdio.h>
+main()
+{
+    printf("%%d") ;
+    printf("\n%%c") ;
+    printf("\n%%f") ;
+    printf("\n%%lb") ;
+    getch() ;
+}
