d074ca5bbccfc36e148ee366b7a5d51d3ce5a85f
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-14 14:50:36 +0530

    Add files via upload

---
+++ programme_25.c
@@ -0,0 +1,14 @@
+// This is a programme to print sum of first N natural numbers
+#include<conio.h>
+#include<stdio.h>
+main()
+{
+    int N , i , sum=0 ;
+    printf("Enter any natural number = ") ;
+    scanf("%d",&N) ;
+    for (i=1 ; i<=N ; i++ )
+        sum = sum+i ;
+    printf("The of of first %d natural numbers is = %d ",N , sum ) ;
+    getch() ;
+}
+
