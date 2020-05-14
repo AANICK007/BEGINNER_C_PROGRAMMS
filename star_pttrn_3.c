7094dfd3f2588c1007d10908322f36a9592d30da
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-14 18:48:24 +0530

    Add files via upload

---
+++ star_pttrn_3.c
@@ -0,0 +1,25 @@
+// This is a programme to  print star pattern
+/*        *
+         **
+        ***
+       ****
+      *****
+*/
+#include<conio.h>
+#include<stdio.h>
+main()
+{
+    int i , j ;
+    for( i=1 ; i<=5 ; i++ )
+    {
+        for( j=1 ; j<=5 ; j++ )
+        {
+            if ( j >= 6-i )
+                printf("*") ;
+            else
+                printf(" ") ;
+        }
+        printf("\n") ;
+    }
+    getch() ;
+}
