ac74bf0d1f5e1198bf28f55150c957c63bff94d5
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-14 14:24:30 +0530

    Add files via upload

---
+++ programme_19.c
@@ -0,0 +1,10 @@
+// This is a programm to print first 10 natural numbers
+#include<conio.h>
+#include<stdio.h>
+main()
+{
+    int i ;
+    for (i=1 ; i<=10 ; i++ )
+        printf("%d ", i) ;
+    getch() ;
+}
