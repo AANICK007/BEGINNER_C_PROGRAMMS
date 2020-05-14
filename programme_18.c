83f3915441cc7c550cf21322f8c5b411b5349c85
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-14 14:18:38 +0530

    Add files via upload

---
+++ programme_18.c
@@ -0,0 +1,11 @@
+// This is a programme to print name 5 times
+#include<conio.h>
+#include<stdio.h>
+main()
+{
+    int i ;
+    for(i=0; i<=4 ; i++ )
+        printf("DHONI  ") ;
+    getch() ;
+}
+
