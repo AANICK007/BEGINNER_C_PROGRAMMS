fae4b7b6153d1833b6c28a8db12d4f05b0f27534
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-14 14:28:45 +0530

    Add files via upload

---
+++ programme_20.c
@@ -0,0 +1,10 @@
+// This is a programm to print first 10 natural  numbers in reverse order
+#include<conio.h>
+#include<stdio.h>
+main()
+{
+    int i ;
+    for (i=10 ; i>=1 ; i-- )
+        printf("%d ", i) ;
+    getch() ;
+}
