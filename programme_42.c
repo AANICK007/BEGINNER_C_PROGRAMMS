6b5c12b2ce9346d82c6f46544b34ac3feeed2853
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-20 18:54:05 +0530

    Add files via upload

---
+++ programme_42.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int Fact (int) ;
+int main(){
+	int N , F ;
+	printf("Enter a natural number = ");
+	scanf("%d",&N) ;
+	F = Fact(N) ;
+	printf("The factorial of %d is %d",N , F);
+	return 0 ;
+}
+
+int Fact(int n){
+	int fact=1 ;
+	if (n>0)
+	{
+		fact = n*Fact(n-1) ;
+	}
+	else{
+	    return 1 ;
+	}
+	return (fact) ;
+}
